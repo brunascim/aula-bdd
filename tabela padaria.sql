2f0fd5dad90698d6e8f2334a58ecb1ceff22a0aa
brunascim <nascimentobruna109@gmail.com>
2026-05-07 19:56:20 -0300

    commit

--- tabela padaria.sql
+++ tabela padaria.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE 04102_Padaria_Bruna;
 USE 04102_Padaria_Bruna;
 SHOW DATABASES;
+SELECT * FROM produtos, vendas 
 
 drop database 04102_Padaria_Bruna;
 
@@ -17,7 +18,6 @@ CREATE TABLE vendas(
     valor DECIMAL(4,2),
     data_venda DATE
 );
-
 INSERT INTO produtos(nome_produtos, preco) VALUES
 ('Bolo de Fubá', 15.00),
 ('Bolo de Chocolate', 20.00),
@@ -31,3 +31,29 @@ INSERT INTO vendas(nome_produtos, quantidade, valor, data_venda) VALUES
 ('Bolo de Fubá', 1, 15.00, '2025-06-04'),
 ('Bolo de Chocolate', 2, 40.00, '2025-06-05'),
 ('Bolo de Milho', 4, 64.00, '2025-06-03');
+
+-- Listar todos os produtos cadastrados 
+SELECT * FROM produtos;
+
+-- Listar total de unidades vendidas por produto SUM
+SELECT nome_produtos, SUM(quantidade) AS total_unidades  -- as total unidades é tipo um complemento antes da resposta
+FROM vendas 
+GROUP BY nome_produtos; 
+
+-- Listar o total de vendas do Bolo de fubá
+SELECT SUM(valor) AS total_vendas
+FROM vendas
+WHERE nome_produtos = 'Bolo de Fubá';
+
+-- Listar o total de vendas do Bolo de Chocolate
+SELECT SUM(valor) AS total_vendas
+FROM vendas 
+WHERE nome_produtos = 'Bolo de Chocolate';
+
+-- Produtos vendidos em 2025-06-03
+SELECT nome_produtos, quantidade, valor
+FROM vendas
+WHERE data_venda = '2025-06-03'
+
+
+
